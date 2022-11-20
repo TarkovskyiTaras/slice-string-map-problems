a8aaf29e0a023ea1f03268559ce75b7c210260d1
Taras Tarkovskyi <tarkovskyi.t@gmail.com>
2022-11-20 15:35:56 +0200

    problem-2.6.2.3 solution addded

--- week2-problems/problem-2.6.2.3/problem2.6.2.3.go
+++ week2-problems/problem-2.6.2.3/problem2.6.2.3.go
@@ -2,28 +2,49 @@ package main
 
 import "fmt"
 
-// Given an array of ints, return true if the array contains no 1's and no 3's.
+/*
+Given an array of ints, return true if the array contains no 1's and no 3's.
 
-func noOneOrThreeInSlice(givenSlice []int) {
+lucky13([0, 2, 4]) → true
+lucky13([1, 2, 3]) → false
+lucky13([1, 2, 4]) → true
+*/
 
-	unluckyElement1 := 1
-	unluckyElement2 := 3
-	var luckMeasurer bool
-
-	for _, c := range givenSlice {
-
-		if c != unluckyElement1 && c != unluckyElement2 {
-			luckMeasurer = true
+func sliceHasNoOnesAndThrees(givenSlice []int) bool {
+	isOne, isThree := false, false
+	for i, gs := range givenSlice {
+		switch gs {
+		case 1:
+			isOne = true
+		case 3:
+			isThree = true
 		}
-		if c == unluckyElement1 || c == unluckyElement2 {
-			luckMeasurer = false
-			break
+		if i > 1 && isOne == true && isThree == true {
+			return false
 		}
 	}
-	fmt.Println(luckMeasurer)
+	return true
 }
 
 func main() {
-	givenSlice := []int{11, 2, 4, 13, 12}
-	noOneOrThreeInSlice(givenSlice)
+	fmt.Println(sliceHasNoOnesAndThrees([]int{2, 2, 2, 3, 4, 2, 1})) //false
 }
+
+//func noOneOrThreeInSlice(givenSlice []int) {
+//
+//	unluckyElement1 := 1
+//	unluckyElement2 := 3
+//	var luckMeasurer bool
+//
+//	for _, c := range givenSlice {
+//
+//		if c != unluckyElement1 && c != unluckyElement2 {
+//			luckMeasurer = true
+//		}
+//		if c == unluckyElement1 || c == unluckyElement2 {
+//			luckMeasurer = false
+//			break
+//		}
+//	}
+//	fmt.Println(luckMeasurer)
+//}
