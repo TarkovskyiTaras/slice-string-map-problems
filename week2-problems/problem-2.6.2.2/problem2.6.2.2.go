14fb88e68746d74bd20336e681895050d9634665
Taras Tarkovskyi <tarkovskyi.t@gmail.com>
2022-11-20 14:38:07 +0200

    problem-2.6.2.2 solution added

--- week2-problems/problem-2.6.2.2/problem2.6.2.2.go
+++ week2-problems/problem-2.6.2.2/problem2.6.2.2.go
@@ -2,33 +2,53 @@ package main
 
 import "fmt"
 
-// Return the sum of the numbers in the array, returning 0 for an empty array.
-//Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+/*
+https://codingbat.com/prob/p127384
 
-func sumOfSliceElements(givenSlice []int) {
-	sumOfEl := 0
-	unluckyNumb := 13
-	for i, c := range givenSlice {
-		lastIndex := len(givenSlice) - 1
-		sumOfEl += c
+Return the sum of the numbers in the array, returning 0 for an empty array.
+Except the number 13 is very unlucky, so it does not count and numbers
+that come immediately after a 13 also do not count.
 
-		if c == unluckyNumb && i == lastIndex {
-			sumOfEl -= givenSlice[lastIndex]
+sum13([1, 2, 2, 1]) → 6
+sum13([1, 1]) → 2
+sum13([1, 2, 2, 1, 13]) → 6
+*/
 
+func sumOfSliceElementsNo13(givenSlice []int) int {
+	sumOfElements := 0
+
+	for i := range givenSlice {
+		if givenSlice[i] == 13 {
+			return sumOfElements
 		}
-
-		if c == unluckyNumb && i != lastIndex {
-			sumOfEl -= c
-
-			sumOfEl -= givenSlice[i+1]
-
-		}
-
+		sumOfElements += givenSlice[i]
 	}
-	fmt.Println(sumOfEl)
+	return sumOfElements
 }
 
 func main() {
-	givenSlice := []int{100, 100, 13, 100, 100, 100, 100, 100, 100, 13}
-	sumOfSliceElements(givenSlice)
+	fmt.Println(sumOfSliceElementsNo13([]int{1, 1, 1, 1, 1, 1, 1, 13, 1, 1, 1})) //7
 }
+
+//func sumOfSliceElements(givenSlice []int) {
+//	sumOfEl := 0
+//	unluckyNumb := 13
+//	for i, c := range givenSlice {
+//		lastIndex := len(givenSlice) - 1
+//		sumOfEl += c
+//
+//		if c == unluckyNumb && i == lastIndex {
+//			sumOfEl -= givenSlice[lastIndex]
+//
+//		}
+//
+//		if c == unluckyNumb && i != lastIndex {
+//			sumOfEl -= c
+//
+//			sumOfEl -= givenSlice[i+1]
+//
+//		}
+//
+//	}
+//	fmt.Println(sumOfEl)
+//}
