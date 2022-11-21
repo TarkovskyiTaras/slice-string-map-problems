472c35f93538d53c1123fed20bfe4a005696c250
Taras Tarkovskyi <tarkovskyi.t@gmail.com>
2022-11-21 13:39:15 +0200

    problem-2.6.2.4 new solution added

--- week2-problems/problem-2.6.2.4/problem2.6.2.4.go
+++ week2-problems/problem-2.6.2.4/problem2.6.2.4.go
@@ -2,19 +2,34 @@ package main
 
 import "fmt"
 
-//Given a number n, create and return a new int array of length n, containing the numbers 0, 1, 2, ... n-1.
+/*
+Given a number n, create and return a new int slice of length n, containing the numbers 0, 1, 2, ... n-1.
+The given n may be 0, in which case just return a length 0 slice.
 
-func fizzSlice(numbOfSliceEl int) {
-	resultSlice := make([]int, numbOfSliceEl)
-	elementValueSpacing := 0
-	for i, _ := range resultSlice {
-		resultSlice[i] = elementValueSpacing
-		elementValueSpacing++
+fizzSlice(4) → [0, 1, 2, 3]
+fizzSlice(1) → [0]
+fizzSlice(10) → [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+*/
 
+func sliceMaker(numbOfElements int) []int {
+	resultSlice := make([]int, numbOfElements, numbOfElements*2)
+	for i := range resultSlice {
+		resultSlice[i] = i
 	}
-	fmt.Println(resultSlice)
+	return resultSlice
 }
 
 func main() {
-	fizzSlice(1000)
+	fmt.Println(sliceMaker(5)) //[0 1 2 3 4]
 }
+
+//func fizzSlice(numbOfSliceEl int) {
+//	resultSlice := make([]int, numbOfSliceEl)
+//	elementValueSpacing := 0
+//	for i, _ := range resultSlice {
+//		resultSlice[i] = elementValueSpacing
+//		elementValueSpacing++
+//
+//	}
+//	fmt.Println(resultSlice)
+//}
