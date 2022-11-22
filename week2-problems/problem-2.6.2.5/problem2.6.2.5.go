8ea3623289ec82761476c0ebee71b2545fc866c6
Taras Tarkovskyi <tarkovskyi.t@gmail.com>
2022-11-22 16:22:08 +0200

    problem-2.6.2.5 new solution added

--- week2-problems/problem-2.6.2.5/problem2.6.2.5.go
+++ week2-problems/problem-2.6.2.5/problem2.6.2.5.go
@@ -2,24 +2,47 @@ package main
 
 import "fmt"
 
-//Given an array of ints, return true if it contains no 1's or it contains no 4's.
+/*
+Given a slice of ints, return true if it contains no 1's and no 4's.
 
-func noOneOrFourInSlice(givenSlice []int) {
-	unluckyNumb1 := 1
-	unluckyNumb2 := 4
-	luckMeasurer := true
-	for _, c := range givenSlice {
-		if c == unluckyNumb1 || c == unluckyNumb2 {
-			luckMeasurer = false
-			break
-		}
+no14([1, 2, 3]) → true
+no14([1, 2, 3, 4]) → false
+no14([2, 3, 4]) → true
+*/
 
+func noOneOrFourInSlice(givenSlice []int) bool {
+	if len(givenSlice) < 2 {
+		return true
 	}
-	fmt.Println(luckMeasurer)
+	var isOne, isFour bool
+	for _, gs := range givenSlice {
+		switch gs {
+		case 1:
+			isOne = true
+		case 4:
+			isFour = true
+		}
+		if isOne && isFour {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-	givenSlice := []int{11, 2, 3, 14, 5, 6}
-	noOneOrFourInSlice(givenSlice)
-
+	fmt.Println(noOneOrFourInSlice([]int{1, 2, 3, 4, 5})) //false
 }
+
+//func noOneOrFourInSlice(givenSlice []int) {
+//	unluckyNumb1 := 1
+//	unluckyNumb2 := 4
+//	luckMeasurer := true
+//	for _, c := range givenSlice {
+//		if c == unluckyNumb1 || c == unluckyNumb2 {
+//			luckMeasurer = false
+//			break
+//		}
+//
+//	}
+//	fmt.Println(luckMeasurer)
+//}
