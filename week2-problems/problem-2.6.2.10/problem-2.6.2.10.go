07adccc04aa2acf0c25aeb96e3de73f0662313ca
taras_mac <tarkovskyi.t@gmail.com>
2022-11-26 02:09:11 +0200

    problem-2.6.2.10 folders added

---
+++ week2-problems/problem-2.6.2.10/problem-2.6.2.10.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+Given a non-empty slice of ints, return a new slice containing the elements from the original slice that come after
+the last 4 in the original slice. The original slice will contain at least one 4.
+
+post4([2, 4, 1, 2]) → [1, 2]
+post4([4, 1, 4, 2]) → [2]
+post4([4, 4, 1, 2, 3]) → [1, 2, 3]
+*/
+
+func post4Slice(slice []int) []int {
+
+}
+
+func main() {
+
+}
