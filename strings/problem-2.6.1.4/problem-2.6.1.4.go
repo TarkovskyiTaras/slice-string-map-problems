6d324dea7b66cb0939ed8b92c2e4af23f8837556
taras_mac <tarkovskyi.t@gmail.com>
2022-12-06 23:29:51 +0200

    problem-2.6.1.4 solution added

--- strings/problem-2.6.1.4/problem-2.6.1.4.go
+++ strings/problem-2.6.1.4/problem-2.6.1.4.go
@@ -2,23 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 //Given a string and an int n, return a string made of n repetitions of the last n characters of the string.
 //You may assume that n is between 0 and the length of the string, inclusive.
 
-func repeatEnd(givenString string, numbOfRep int) {
-
-	endString := givenString[len(givenString)-numbOfRep:]
-	var endStringNTimes string
-	for x := 1; x <= numbOfRep; x++ {
-		endStringNTimes += endString
+func endString(givenString string, repNumb uint) string {
+	if int(repNumb) > len(givenString) {
+		log.Fatal("invalid data passed")
 	}
-	fmt.Println(endStringNTimes)
+	var resultString string
+	for i := 1; i <= int(repNumb); i++ {
+		resultString += givenString[len(givenString)-int(repNumb):]
+	}
+	return resultString
 }
 
 func main() {
-	givenString := "123456789T"
-	numbOfRep := 5
-	repeatEnd(givenString, numbOfRep)
+	fmt.Println(endString("taras", 4)) // "arasarasarasaras"
 }
+
+//func repeatEnd(givenString string, numbOfRep int) {
+//
+//	endString := givenString[len(givenString)-numbOfRep:]
+//	var endStringNTimes string
+//	for x := 1; x <= numbOfRep; x++ {
+//		endStringNTimes += endString
+//	}
+//	fmt.Println(endStringNTimes)
+//}
