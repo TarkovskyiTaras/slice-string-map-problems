7e82ce571950538680628ce28a9d06b53cd2eae9
taras_mac <tarkovskyi.t@gmail.com>
2022-12-14 18:22:48 +0200

    folder added

---
+++ strings/problem-2.6.1.6/problem-2.6.1.6.go
@@ -0,0 +1,9 @@
+package main
+
+/*
+Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the star, they are the same.
+
+sameStarChar("xy*yzz") → true
+sameStarChar("xy*zzz") → false
+sameStarChar("*xa*az") → true
+*/
