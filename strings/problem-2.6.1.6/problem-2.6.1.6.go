0bf57d942f0df148fc3a98485384b6ab7e9a765c
taras_mac <tarkovskyi.t@gmail.com>
2022-12-15 13:55:49 +0200

    problem-2.6.1.6 solution added

--- strings/problem-2.6.1.6/problem-2.6.1.6.go
+++ strings/problem-2.6.1.6/problem-2.6.1.6.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the star, they are the same.
 
@@ -7,3 +9,21 @@ sameStarChar("xy*yzz") → true
 sameStarChar("xy*zzz") → false
 sameStarChar("*xa*az") → true
 */
+
+func sameStarChar(givenString string) bool {
+	sameCharAstr := false
+	for i := 1; i < len(givenString)-1; i++ {
+		if string(givenString[i]) == "*" {
+			if givenString[i-1] != givenString[i+1] {
+				return false
+			} else {
+				sameCharAstr = true
+			}
+		}
+	}
+	return sameCharAstr
+}
+
+func main() {
+	fmt.Println(sameStarChar("tas*s"))
+}
