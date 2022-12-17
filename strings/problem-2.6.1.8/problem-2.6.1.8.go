52106fa5792d650e5281c180b06e09532aeee6b9
taras_mac <tarkovskyi.t@gmail.com>
2022-12-17 13:07:01 +0200

    problem-2.6.1.8 solution added

--- strings/problem-2.6.1.8/problem-2.6.1.8.go
+++ strings/problem-2.6.1.8/problem-2.6.1.8.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 Return the number of times that the string "hi" appears anywhere in the given string.
 
@@ -7,3 +9,18 @@ countHi("abc hi ho") → 1
 countHi("ABChi hi") → 2
 countHi("hihi") → 2
 */
+
+func countHi(s, substring string) int {
+	matchCount := 0
+	t := len(s) - len(substring) + 1
+	for i := 0; i < t; i++ {
+		if s[i:i+len(substring)] == substring {
+			matchCount++
+		}
+	}
+	return matchCount
+}
+
+func main() {
+	fmt.Println(countHi("tarasnikitatarasikitataras", "taras"))
+}
