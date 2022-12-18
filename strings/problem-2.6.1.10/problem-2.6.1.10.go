021435e08b278fe620a10bc080ecfab77e129ca8
taras_mac <tarkovskyi.t@gmail.com>
2022-12-18 08:41:29 +0200

    problem-2.6.1.10 solution + test added

--- strings/problem-2.6.1.10/problem-2.6.1.10.go
+++ strings/problem-2.6.1.10/problem-2.6.1.10.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char somewhere later in the string.
 So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's. Return true if the given string is xy-balanced.
@@ -8,3 +10,23 @@ xyBalance("aaxbby") → true
 xyBalance("aaxbb") → false
 xyBalance("yaaxbb") → false
 */
+func xyBalance(s string) bool {
+	isXRight := false
+	isY := false
+	for j := len(s) - 1; j >= 0; j-- {
+		if s[j] == 'x' {
+			isXRight = true
+		}
+		if s[j] == 'y' && !isXRight {
+			isY = true
+		}
+	}
+	if isXRight && isY {
+		return true
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(xyBalance("tayxasyt"))
+}
