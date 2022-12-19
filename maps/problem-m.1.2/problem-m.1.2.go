71eec81c2c88231bdee70b67942cdd34c59435e9
taras_mac <tarkovskyi.t@gmail.com>
2022-12-19 09:13:02 +0200

    problem-m.1.2

--- maps/problem-m.1.2/problem-m.1.2.go
+++ maps/problem-m.1.2/problem-m.1.2.go
@@ -1,9 +1,24 @@
-package problem_m_1_2
+package main
+
+import "fmt"
 
 /*
-Given a map of food keys and topping values, modify and return the map as follows: if the key "ice cream" is present, set its value to "cherry". In all cases, set the key "bread" to have the value "butter".
+Given a map of food keys and topping values, modify and return the map as follows: if the key "ice cream" is present,
+set its value to "cherry". In all cases, set the key "bread" to have the value "butter".
 
 topping1({"ice cream": "peanuts"}) → {"bread": "butter", "ice cream": "cherry"}
 topping1({}) → {"bread": "butter"}
 topping1({"pancake": "syrup"}) → {"bread": "butter", "pancake": "syrup"}
 */
+
+func topping1(m map[string]string) map[string]string {
+	if _, ok := m["ice cream"]; ok {
+		m["ice cream"] = "cherry"
+	}
+	m["bread"] = "butter"
+	return m
+}
+
+func main() {
+	fmt.Println(topping1(map[string]string{"ice cream": "vanilla", "pancake": "syrup"}))
+}
