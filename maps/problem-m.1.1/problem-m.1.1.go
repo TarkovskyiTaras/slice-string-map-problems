aff35ad36cb113115ce6980f01f419f43e2026f2
taras_mac <tarkovskyi.t@gmail.com>
2022-12-19 09:02:50 +0200

    problem-m.1.1 solution added

--- maps/problem-m.1.1/problem-m.1.1.go
+++ maps/problem-m.1.1/problem-m.1.1.go
@@ -1,9 +1,28 @@
-package problem_m_1_1
+package main
+
+import "fmt"
 
 /*
-Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value, and set the key "a" to have the value "". Basically "b" is a bully, taking the value and replacing it with the empty string.
+Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value,
+and set the key "a" to have the value "". Basically "b" is a bully, taking the value and replacing it with the empty string.
 
 mapBully({"a": "candy", "b": "dirt"}) → {"a": "", "b": "candy"}
 mapBully({"a": "candy"}) → {"a": "", "b": "candy"}
 mapBully({"a": "candy", "b": "carrot", "c": "meh"}) → {"a": "", "b": "candy", "c": "meh"}
 */
+
+func mapBully(m map[string]string) map[string]string {
+	if m == nil {
+		m = make(map[string]string)
+	}
+	if x, ok := m["a"]; ok {
+		m["b"] = x
+		m["a"] = ""
+	}
+	return m
+}
+
+func main() {
+	fmt.Println(mapBully(map[string]string{"a": "taras", "b": "nikita"})) // map[a: b:taras]
+	fmt.Println(mapBully(map[string]string{"b": "123", "c": "456"}))      // map[b:123 c:456]
+}
