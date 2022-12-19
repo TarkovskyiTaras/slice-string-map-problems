edd8501e1fd2fca8abbfa1521e066e6f862ebc1c
taras_mac <tarkovskyi.t@gmail.com>
2022-12-19 10:14:49 +0200

    problem-m.1.3 solution added

--- maps/problem-m.1.3/problem-m.1.3.go
+++ maps/problem-m.1.3/problem-m.1.3.go
@@ -1,4 +1,6 @@
-package problem_m_1_3
+package main
+
+import "fmt"
 
 /*
 Modify and return the given map as follows: if the keys "a" and "b" are both in the map and have equal values, remove them both.
@@ -7,3 +9,18 @@ mapAB2({"a": "aaa", "b": "aaa", "c": "cake"}) → {"c": "cake"}
 mapAB2({"a": "aaa", "b": "bbb"}) → {"a": "aaa", "b": "bbb"}
 mapAB2({"a": "aaa", "b": "bbb", "c": "aaa"}) → {"a": "aaa", "b": "bbb", "c": "aaa"}
 */
+
+func mapAB2(m map[string]string) map[string]string {
+	ma, ok1 := m["a"]
+	mb, ok2 := m["b"]
+	if ok1 && ok2 && ma == mb {
+		delete(m, "a")
+		delete(m, "b")
+	}
+	return m
+}
+
+func main() {
+	fmt.Println(mapAB2(map[string]string{"a": "1", "c": "2"}))
+	fmt.Println(mapAB2(map[string]string{"a": "1", "b": "1", "c": "3"}))
+}
