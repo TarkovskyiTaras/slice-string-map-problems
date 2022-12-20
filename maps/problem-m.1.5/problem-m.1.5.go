b4aebe1e3d0754d0b1d3a58fcc6f7b3f37b3306b
taras_mac <tarkovskyi.t@gmail.com>
2022-12-20 09:21:20 +0200

    problem-m.1.5 solution added

--- maps/problem-m.1.5/problem-m.1.5.go
+++ maps/problem-m.1.5/problem-m.1.5.go
@@ -1,9 +1,27 @@
-package problem_m_1_5
+package main
+
+import "fmt"
 
 /*
-Given a map of food keys and their topping values, modify and return the map as follows: if the key "ice cream" has a value, set that as the value for the key "yogurt" also. If the key "spinach" has a value, change that value to "nuts".
+Given a map of food keys and their topping values, modify and return the map as follows: if the key "ice cream" has a value,
+set that as the value for the key "yogurt" also. If the key "spinach" has a value, change that value to "nuts".
 
 topping2({"ice cream": "cherry"}) → {"yogurt": "cherry", "ice cream": "cherry"}
 topping2({"spinach": "dirt", "ice cream": "cherry"}) → {"yogurt": "cherry", "spinach": "nuts", "ice cream": "cherry"}
 topping2({"yogurt": "salt"}) → {"yogurt": "salt"}
 */
+
+func topping2(m map[string]string) map[string]string {
+	if _, ok := m["ice cream"]; ok {
+		m["yogurt"] = m["ice cream"]
+	}
+	if _, ok := m["spinach"]; ok {
+		m["spinach"] = "nuts"
+	}
+	return m
+}
+
+func main() {
+	fmt.Print(topping2(map[string]string{"ice cream": "value for ice cream", "yogurt": "value for yogurt",
+		"spinach": "value for spinach"})) // map[ice cream:value for ice cream spinach:nuts yogurt:value for ice cream]
+}
