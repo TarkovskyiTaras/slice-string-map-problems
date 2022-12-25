15251ef4a0294e014aa0f4d224da6a916384dbc0
taras_mac <tarkovskyi.t@gmail.com>
2022-12-25 10:19:37 +0200

    problem-m.2.2 solution added

--- maps/problem-m.2.2/problem-m.2.2.go
+++ maps/problem-m.2.2/problem-m.2.2.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 The classic word-count algorithm: given an array of strings, return a Map<String, Integer> with a key for each different
 string, with the value the number of times that string appears in the array.
@@ -8,3 +10,15 @@ wordCount(["a", "b", "a", "c", "b"]) → {"a": 2, "b": 2, "c": 1}
 wordCount(["c", "b", "a"]) → {"a": 1, "b": 1, "c": 1}
 wordCount(["c", "c", "c", "c"]) → {"c": 4}
 */
+
+func wordCount(ss []string) map[string]int {
+	resultMap := make(map[string]int)
+	for _, c := range ss {
+		resultMap[c]++
+	}
+	return resultMap
+}
+
+func main() {
+	fmt.Println(wordCount([]string{"a", "a", "b", "b", "c", "c", "c", "c", "d", "d", "e", "f", "f", "f", "g"}))
+} // map[a:2 b:2 c:4 d:2 e:1 f:3 g:1]
